59d1bdc966208827d0a83dc4b0efdaee34568209
dewafer <dewafer@gmail.com>
2016-09-09 12:54:38 +0800

    增加授权方式管理

---
+++ oauth2-server-boot/src/main/java/com/newnil/cas/oauth2/provider/controller/GrantTypeAdminController.java
@@ -0,0 +1,89 @@
+package com.newnil.cas.oauth2.provider.controller;
+
+import com.newnil.cas.oauth2.provider.dao.entity.ClientDetailsToAuthorizedGrantTypeXrefEntity;
+import com.newnil.cas.oauth2.provider.dao.entity.GrantTypeEntity;
+import com.newnil.cas.oauth2.provider.dao.repository.GrantTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import static com.newnil.cas.oauth2.provider.webhelper.RedirectMessageHelper.*;
+
+@Controller
+@RequestMapping("/grantTypes")
+@PreAuthorize("hasRole('ROLE_ADMIN')")
+public class GrantTypeAdminController {
+
+    @Autowired
+    private GrantTypeRepository grantTypeRepository;
+
+    @RequestMapping(method = RequestMethod.GET, produces = {MediaType.TEXT_HTML_VALUE, MediaType.APPLICATION_XHTML_XML_VALUE})
+    public String listAllGrantTypes(Model model, Pageable pageable) {
+
+        model.addAttribute("grantTypes", grantTypeRepository.findAll(pageable));
+        return "clients/grantTypes";
+    }
+
+    private static final Pattern GRANT_TYPES_PATTERN = Pattern.compile("^[a-zA-Z_]+$");
+
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            produces = {MediaType.TEXT_HTML_VALUE, MediaType.APPLICATION_XHTML_XML_VALUE})
+    public String createGrantType(@RequestParam("grantTypeName") String grantTypeName, RedirectAttributes attributes) {
+
+        if (GRANT_TYPES_PATTERN.matcher(grantTypeName).matches()) {
+            if (grantTypeRepository.findOneByValue(grantTypeName.toLowerCase()).isPresent()) {
+                // error message
+                addErrorMessage(attributes, "授权方式 " + grantTypeName + " 已经存在。");
+                attributes.addFlashAttribute("grantTypeName", grantTypeName);
+            } else {
+                grantTypeRepository.save(GrantTypeEntity.builder().value(grantTypeName.toLowerCase()).build());
+                // success message
+                addSuccessMessage(attributes, "已成功添加 " + grantTypeName + " 授权方式。");
+            }
+        } else {
+            addErrorMessage(attributes, "授权方式 " + grantTypeName + " 含有非法字符。（只能使用[a-zA-Z_]）");
+            attributes.addFlashAttribute("grantTypeName", grantTypeName);
+        }
+
+        return "redirect:/grantTypes";
+    }
+
+    @RequestMapping(path = "/_remove/{grantTypeName}", method = RequestMethod.GET, produces = {MediaType.TEXT_HTML_VALUE, MediaType.APPLICATION_XHTML_XML_VALUE})
+    public String removeGrantType(@PathVariable("grantTypeName") String grantTypeName, RedirectAttributes attributes) {
+
+        grantTypeRepository.findOneByValue(grantTypeName.toLowerCase()).map(
+                grantTypeEntity -> {
+
+                    Set<ClientDetailsToAuthorizedGrantTypeXrefEntity> xref = grantTypeEntity.getClientDetailsToAuthorizedGrantTypeXrefs();
+                    if (xref.isEmpty()) {
+
+                        grantTypeRepository.delete(grantTypeEntity);
+                        addSuccessMessage(attributes, "已成功删除 " + grantTypeName + " 授权方式。");
+
+                    } else {
+                        addErrorMessage(attributes, "有" + xref.size() + "个客户端正在使用该授权方式，无法删除。");
+                    }
+
+                    return grantTypeEntity;
+
+                }
+        ).orElseGet(() -> {
+            addWarningMessage(attributes, "没有找到 " + grantTypeName + " 授权方式。");
+            return null;
+        });
+
+        return "redirect:/grantTypes";
+    }
+
+}

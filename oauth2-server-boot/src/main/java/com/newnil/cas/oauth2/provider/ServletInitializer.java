08487a2c16fb5ad85aeaa4ce8cf509efd5b1542d
dewafer <dewafer@gmail.com>
2017-01-06 10:20:34 +0800

    新调教

---
+++ oauth2-server-boot/src/main/java/com/newnil/cas/oauth2/provider/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.newnil.cas.oauth2.provider;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Oauth2ServerBootApplication.class);
+	}
+
+}
